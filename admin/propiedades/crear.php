0768fe41d7862862fc2a62b570a887fe24ab9e13
EstebanToloza <estebanisaiastoloza@gmail.com>
2022-11-13 18:38:52 -0300

    Remove ignored files

---
+++ admin/propiedades/crear.php
@@ -0,0 +1,70 @@
+<?php
+
+//base de datos
+
+require "../../includes/config/database.php";
+$db = conectarDB();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";
+}
+//
+require '../../includes/funciones.php';
+
+incluirTemplate('header')
+?>
+
+<main class="contenedor seccion">
+    <h1>Crear</h1>
+    <a href="/admin" class="boton boton-verde">Volver</a>
+
+    <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
+        <fieldset>
+            <legend>Información General</legend>
+
+            <label for="título" class="">Título</label>
+            <input type="text" name="titulo" id="titulo" placeholder="Título propiedad">
+
+            <label for="precio" class="">Precio</label>
+            <input type="number" name="precio" id="precio" placeholder="Precio propiedad">
+
+            <label for="imagen" class="">Imagen</label>
+            <input type="file" name="imagen" id="imagen" accept="image/jpeg, image/png">
+
+            <label for="descripcion" class="">Descripción</label>
+            <textarea type="text" name="descripcion" id="descripcion" placeholder="Descripción propiedad"></textarea>
+
+        </fieldset>
+        <fieldset>
+            <legend>Información Propiedad</legend>
+
+            <label for="habitaciones" class="">Habitaciones</label>
+            <input type="number" name="habitaciones" id="habitaciones" placeholder="Ej: 3" min="1" max="10">
+
+            <label for="wc" class="">Baños</label>
+            <input type="number" name="wc" id="wc" placeholder="Ej: 3" min="1" max="10">
+
+            <label for="estacionamiento" class="">Estacionamiento</label>
+            <input type="number" name="estacionamiento" id="estacionamiento" placeholder="Ej: 3" min="1" max="10">
+
+        </fieldset>
+
+        <fieldset>
+            <legend>Vendedor</legend>
+
+            <select>
+                <option value="1">Juan</option>
+                <option value="2">Diego</option>
+            </select>
+        </fieldset>
+
+        <input type="submit" value="Crear Propiedad" class="boton boton-verde" />
+    </form>
+
+</main>
+
+<?php
+incluirTemplate('footer')
+?>
